c6dfbbf249b26a2ae590923327bd4bfec2282610
Mark Calabretta <mcalabre@atnf.csiro.au>
2006-09-11 08:08:40 +0000

    Old Miriad archive file. File last modification timestamp: 19900428   Date recorded in the file name: 19900428

---
+++ subs/model.h
@@ -0,0 +1,8 @@
+	integer maxcals
+	parameter(maxcals=32)
+	integer ncals,calcur
+	logical planet(maxcals)
+	character cals(maxcals)*16
+	real calflux(maxcals),CalFreq(maxcals)
+	common/calcom/ncals,calcur,calflux,calfreq,planet
+	common/calcomc/cals
