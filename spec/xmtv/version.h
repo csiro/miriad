4dbcc253b4b0d2e8dfc1f07ab15635df6fc8a094
Mark Calabretta <mcalabre@atnf.csiro.au>
2006-09-15 07:19:52 +0000

    Old Miriad archive file. File last modification timestamp: 19970822 From an incremental backup dated: 19970828

--- spec/xmtv/version.h
+++ spec/xmtv/version.h
@@ -2,5 +2,6 @@
  *	<version.h> - Define current version of MXTV - MIRIAD version.
  *	01jul92 jm  Original version.
  *	13nov92 jm  version 1.1.
+ *	19dec95 jm  version 1.2.
  */
-#define VERSION "Version 1.1 13-nov-92"
+#define VERSION "Version 1.2 19-dec-95"
