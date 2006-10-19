c7d5ca34f23267a9dad7fdb9d9ce2903925672b8
Mark Calabretta <mcalabre@atnf.csiro.au>
2006-10-19 06:16:47 +0000

    Include stdlib.h to eliminate compiler warnings.

--- tools/tract.c
+++ tools/tract.c
@@ -1,32 +1,4 @@
 /*----------------------------------------------------------------------------
-* tract: Report the age of a file or directory.
-*-----------------------------------------------------------------------------
-*
-*   Copyright (C) 1995
-*   Associated Universities, Inc. Washington DC, USA.
-*
-*   This program is free software; you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation; either version 2 of the License, or
-*   (at your option) any later version.
-*
-*   This program is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with this program; if not, write to the Free Software
-*   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*
-*   Correspondence concerning AIPS++ should be addressed as follows:
-*          Internet email: aips2-request@nrao.edu.
-*          Postal address: AIPS++ Project Office
-*                          National Radio Astronomy Observatory
-*                          520 Edgemont Road
-*                          Charlottesville, VA 22903-2475 USA
-*
-*-----------------------------------------------------------------------------
 * Usage: tract [-a | -c | -m ] [-s] [-q#] <file|directory>
 *-----------------------------------------------------------------------------
 * tract reports the age of a file or directory; by default it reports the time
@@ -49,15 +21,16 @@
 *    2:  File access error.
 *
 * Original: 1995/02/21 by Mark Calabretta, ATNF
-* $Id: tract.c,v 1.1 2006/09/12 01:50:44 cal103 Exp $
+* $Id: tract.c,v 1.2 2006/10/19 06:16:47 cal103 Exp $
 *---------------------------------------------------------------------------*/
 
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/time.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <errno.h>
 
 char *usage="Usage: tract [-a | -c | -m] [-s] [-q#] <file|directory>";
 
