0f6d664b913e2012ac2362222c278f24ea1db315
Mark Calabretta <mcalabre@atnf.csiro.au>
2007-10-30 08:26:38 +0000

    Old Miriad archive file. File last modification timestamp: 19950809   Date recorded in the file name: 19971216

--- subs/key.h
+++ subs/key.h
@@ -1,11 +1,7 @@
-c  Common block for key.for
-c  Last mod:    pjt	10-may-90	added argv0
-c               pjt     16-mar-91       added -h option, needs qhelp
-c		pjt      8-nov-91	more bufferspace
 	integer maxlen,maxkeys
 	parameter(maxlen=7000,maxkeys=32)
-	character pbuf*(maxlen),keys(maxkeys)*8
+	character pbuf*(maxlen),keys(maxkeys)*8,argv0*32
 	integer lu(maxkeys),k1(maxkeys),k2(maxkeys),nkeys
 	logical expanded(maxkeys),qhelp
-	common/keycommc/pbuf,keys
+	common/keycommc/pbuf,argv0,keys
 	common/keycomm/ lu,k1,k2,expanded,nkeys,qhelp
