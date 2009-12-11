6ce34886313fe0c7f1cfcbaa682ca0871fb5601a
Mark Wieringa <Mark.Wieringa@csiro.au>
2009-12-11 04:13:29 +0000

    increase flag count limits by factor 10

--- prog/tvclip.for
+++ prog/tvclip.for
@@ -257,7 +257,7 @@ c    rjs   21sep00    options=nosrc
 c    jwr   16jun04    initialize nedit=0 in doEdit
 c    jwr   16jun04    increased MAXEDIT
 c
-c $Id: tvclip.for,v 1.6 2009/09/23 01:00:20 cal103 Exp $
+c $Id: tvclip.for,v 1.7 2009/12/11 04:13:29 wie017 Exp $
 c-----------------------------------------------------------------------
 c
 c  Internal parameters.
@@ -266,7 +266,7 @@ c
       character PROG*(*)
       parameter (PROG = 'TVCLIP: ')
       integer NMODE,MAXSELS,MAXEDIT,MAXCMD
-      parameter (NMODE=4,MAXCMD=10,MAXSELS=256,MAXEDIT=1000000)
+      parameter (NMODE=4,MAXCMD=10,MAXSELS=256,MAXEDIT=10000000)
 c
 c  Internal variables.
 c
@@ -308,8 +308,8 @@ c-----------------------------------------------------------------------
 c  Announce program.
 c
       version = versan ('tvclip',
-     :                  '$Revision: 1.6 $',
-     :                  '$Date: 2009/09/23 01:00:20 $')
+     :                  '$Revision: 1.7 $',
+     :                  '$Date: 2009/12/11 04:13:29 $')
 c-----------------------------------------------------------------------
 c  Use the key routines to get the user input parameters.
 c
@@ -545,7 +545,7 @@ c-----------------------------------------------------------------------
 	integer MAXTIME,MAXTIME2,MAXSAVE,MAXTREV
 	real ttol
 	parameter(MAXTIME=10000,MAXTIME2=MAXDIM,MAXTREV=128,
-     *			MAXSAVE=100000,ttol=1.0/86400.0)
+     *			MAXSAVE=1000000,ttol=1.0/86400.0)
 	integer i,j,k,i1,i2,iblok
 	integer lScr
 	real t1(MAXTIME),t2(MAXTIME)
@@ -2627,7 +2627,7 @@ c  and redisplay the necessary parts that need redisplaying.
 c
 c-----------------------------------------------------------------------
 	integer WIDTH, MAXSAVE, MAXX, MAXY
-	parameter(WIDTH=20, MAXSAVE=100000, MAXX=8192, MAXY=8640)
+	parameter(WIDTH=20, MAXSAVE=1000000, MAXX=8192, MAXY=8640)
         real omedian, chnmed(MAXX), timemed(MAXY), medchn,
      -   medtime, chndev, timedev, buf(MAXY)
         integer chncount(MAXX), timecnt(MAXY)
