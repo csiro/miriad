eb719831d7f4ff6b3cf21c7170e4b4a1397cede5
Mark Calabretta <mcalabre@atnf.csiro.au>
2010-05-11 00:31:39 +0000

    Reestablished the change made at revision 1.12.

--- prog/fits.for
+++ prog/fits.for
@@ -129,6 +129,8 @@ c       Alternately:
 c         velocity=lsr
 c       indicates that fits is to determine the observatory velocity
 c       wrt the LSR frame using an appropriate model.
+c
+c$Id: fits.for,v 1.14 2010/05/11 00:31:39 cal103 Exp $
 c--
 c
 c  Bugs:
@@ -376,12 +378,11 @@ c                    can read SMA FITS output data from IDL.
 c    rjs  09-apr-09  Apply AIPS baseline dependent calibration and
 c                    various cosmetic changes.
 c    rjs  20-jul-09  Slight changes to messages about extension files.
-c    mrc? 09-apr-10  More robust way of getting rest frequency from cards.
-c    rjs  23-apr-10  Handle antenna tables with compacted entries in a fashion to
-c		     preserve antenna numbers (rather than re-numbering). Improve
-c		     frequency precision.
-c
-c $Id: fits.for,v 1.13 2010/05/10 18:24:39 sau078 Exp $
+c    mrc? 09-apr-10  More robust way of getting rest frequency from
+c                    cards.
+c    rjs  23-apr-10  Handle antenna tables with compacted entries in a
+c                    fashion to preserve antenna numbers (rather than
+c                    re-numbering).  Improve frequency precision.
 c-----------------------------------------------------------------------
 	integer maxboxes
 	parameter(maxboxes=2048)
@@ -394,8 +395,8 @@ c-----------------------------------------------------------------------
 	character versan*80, version*80
 c-----------------------------------------------------------------------
       version = versan ('fits',
-     :                  '$Revision: 1.13 $',
-     :                  '$Date: 2010/05/10 18:24:39 $')
+     :                  '$Revision: 1.14 $',
+     :                  '$Date: 2010/05/11 00:31:39 $')
 c
 c  Get the input parameters.
 c
