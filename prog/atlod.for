ad9e0387c7050d4d52c9af514ce808e5fbf1b382
Mark Wieringa <Mark.Wieringa@csiro.au>
2010-06-23 01:17:59 +0000

    add zoom mode flagging

--- prog/atlod.for
+++ prog/atlod.for
@@ -151,7 +151,7 @@ c       skip, followed by the number of scans to process.  NOTE: This
 c       applies to all files read.  The default is to skip none and
 c       process all scans.
 c
-c$Id: atlod.for,v 1.25 2010/04/30 08:10:47 cal103 Exp $
+c$Id: atlod.for,v 1.26 2010/06/23 01:17:59 wie017 Exp $
 c--
 c
 c  Program Structure:
@@ -321,8 +321,8 @@ c
         character itoaf*8, rperr*32, versan*80
 c-----------------------------------------------------------------------
       version = versan ('atlod',
-     :                  '$Revision: 1.25 $',
-     :                  '$Date: 2010/04/30 08:10:47 $')
+     :                  '$Revision: 1.26 $',
+     :                  '$Date: 2010/06/23 01:17:59 $')
 c
 c  Get the input parameters.
 c
@@ -3585,11 +3585,11 @@ c
         if (birdie) then
           offset=1
           do i=1,nifs
+            if (nfreq(i).eq.2049.and.
+     *          abs(abs(sdf(i))-0.001).lt.1.e-4) then
 c          
 c             CABB Mode 2048*1MHz
 c
-            if (nfreq(i).eq.2049.and.
-     *          abs(abs(sdf(i))-0.001).lt.1.e-4) then
               do j=1,NBIRDIE1
                 flags(offset+b1(j))=.false.
               enddo
@@ -3616,6 +3616,19 @@ c
               do j=ch2,nfreq(i)-1
                   flags(offset+j)=.false.
               enddo
+            else if (nfreq(i).ge.2049.and.
+     *          abs(abs(sdf(i))-0.5e-6).lt.1.e-7) then
+c
+c             CABB zoom mode 2049*0.5kHz, or >2049 (blended zooms)
+c  
+              ch1=99
+              ch2=nfreq(i)-100
+              do j=0,ch1
+                flags(offset+j)=.false.
+              enddo
+              do j=ch2,nfreq(i)-1
+                  flags(offset+j)=.false.
+              enddo                      
             endif
             offset=offset+nfreq(i)
           enddo
