ad9738fdebf78ed7be4f49a5ebf30568f3c28339
Mark Wieringa <Mark.Wieringa@csiro.au>
2011-01-25 01:28:01 +0000

    increase MAXVHANDS to 1024

--- subs/uvio.c
+++ subs/uvio.c
@@ -251,8 +251,9 @@
 *  rjs  28jun06 Prototype arguments and some tidying.
 *  rjs  09jul06 uvread_updated_uvw checks no. of antennas.
 *  mhw  08oct07 add selection on seeing using variable smonrms.
+*  mhw  25jan11 increase MAXVHANDS to 1024
 *
-* $Id: uvio.c,v 1.5 2009/08/18 08:23:36 cal103 Exp $
+* $Id: uvio.c,v 1.6 2011/01/25 01:28:01 wie017 Exp $
 *===========================================================================*/
 #define VERSION_ID "14-Feb-08 MRC"
 
@@ -488,7 +489,7 @@ typedef struct {
 	WINDOW *win;
 		} UV;
 
-#define MAXVHANDS 128
+#define MAXVHANDS 1024
 
 static UV *uvs[MAXOPEN];
 static VARHAND *varhands[MAXVHANDS];
