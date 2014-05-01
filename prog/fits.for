b79c798624e7670404ebcda6853d0e36a7e76624
Mark Wieringa <Mark.Wieringa@csiro.au>
2014-05-01 00:40:44 +0000

    cope with HPX rotation

--- prog/fits.for
+++ prog/fits.for
@@ -130,7 +130,7 @@ c         velocity=lsr
 c       indicates that fits is to determine the observatory velocity
 c       wrt the LSR frame using an appropriate model.
 c
-c$Id: fits.for,v 1.27 2013/03/21 21:08:16 sau078 Exp $
+c$Id: fits.for,v 1.28 2014/05/01 00:40:44 wie017 Exp $
 c--
 c
 c  Bugs:
@@ -152,6 +152,7 @@ c  History:
 c    Refer to the RCS log, v1.1 includes prior revision information.
 c    mhw 26oct12  Initialize map rotation to zero
 c    rjs 21mar13  Fix check for buffer overflow in uvin.
+c    mhw 01may14  Cope with HPX rotation
 c
 c-----------------------------------------------------------------------
       integer   MAXBOXES
@@ -167,8 +168,8 @@ c-----------------------------------------------------------------------
       character versan*72
 c-----------------------------------------------------------------------
       version = versan('fits',
-     *                 '$Revision: 1.27 $',
-     *                 '$Date: 2013/03/21 21:08:16 $')
+     *                 '$Revision: 1.28 $',
+     *                 '$Date: 2014/05/01 00:40:44 $')
 c
 c  Get the input parameters.
 c
@@ -4075,17 +4076,19 @@ c-----------------------------------------------------------------------
       call fitrdhdd(lIn,'CROTA'//itoaf(ilat),crota2,0d0)
       crota2 = crota2*DD2R
 
-      call fitrdhdd(lIn,m0ij(pc,ilng,ilng),pc11,cos(crota2))
-      if (crota2.ne.0d0) then
-        call fitrdhdd(lIn,m0ij(pc,ilng,ilat), pc12,
+      call fitrdhdd(lIn,m0ij(pc,ilng,ilng),dtemp,cos(crota2))
+      call fitrdhdd(lIn,mi_j(pc,ilng,ilng),pc11,dtemp)
+      
+      call fitrdhdd(lIn,m0ij(pc,ilng,ilat),dtemp,
      *                  -sin(crota2)*cdelt2/cdelt1)
-        call fitrdhdd(lIn,m0ij(pc,ilat,ilng), pc21,
+      call fitrdhdd(lIn,mi_j(pc,ilng,ilat),pc12,dtemp)
+      
+      call fitrdhdd(lIn,m0ij(pc,ilat,ilng),dtemp,
      *                   sin(crota2)*cdelt1/cdelt2)
-      else
-        pc12 = 0d0
-        pc21 = 0d0
-      endif
-      call fitrdhdd(lIn,m0ij(pc,ilat,ilat),pc22,cos(crota2))
+      call fitrdhdd(lIn,mi_j(pc,ilat,ilng),pc21,dtemp)
+      
+      call fitrdhdd(lIn,m0ij(pc,ilat,ilat),dtemp,cos(crota2))
+      call fitrdhdd(lIn,mi_j(pc,ilat,ilat),pc22,dtemp)
 
       call fitrdhdd(lIn,m0ij(cd,ilng,ilng),dtemp,pc11*cdelt1)
       call fitrdhdd(lIn,mi_j(cd,ilng,ilng),cd11,dtemp)
@@ -4108,7 +4111,7 @@ c     Convert matrix elements to cdelt1, cdelt2, and llrot.
         call bug('w','Using some mean rotation')
       endif
       llrot  = (crota1 + crota2)/2d0
-      if (abs(llrot).lt.1d-300) llrot=0d0
+      if (abs(llrot).lt.1d-30) llrot=0d0
       cdelt1 = cd11/cos(llrot)
       cdelt2 = cd22/cos(llrot)
 
