07b1ceb4c8828f470aef6725e96cdd18a31af345
Mark Wieringa <Mark.Wieringa@csiro.au>
2018-11-15 23:31:35 +0000

    do explicit conversion to real to avoid compiler warnings

--- inc/mirconst.h
+++ inc/mirconst.h
@@ -6,7 +6,7 @@ c    jm  18dec90  Original code.  Constants taken from the paper
 c                 "The Fundamental Physical Constants" by E. Richard
 c                 Cohen and Barry N. Taylor (PHYICS TODAY, August 1989).
 c
-c $Id: mirconst.h,v 1.2 2007/08/13 09:07:47 cal103 Exp $
+c $Id: mirconst.h,v 1.3 2018/11/15 23:31:35 wie017 Exp $
 c ----------------------------------------------------------------------
 c  Pi.
       double precision DPI, DPI_2, DPI_4, DTWOPI
@@ -16,10 +16,10 @@ c  Pi.
       parameter (DPI_4  = DPI / 4D0)
 
       real PI, PI_2, PI_4, TWOPI
-      parameter (PI    = DPI)
-      parameter (TWOPI = DTWOPI)
-      parameter (PI_2  = DPI_2)
-      parameter (PI_4  = DPI_4)
+      parameter (PI    = real(DPI))
+      parameter (TWOPI = real(DTWOPI))
+      parameter (PI_2  = real(DPI_2))
+      parameter (PI_4  = real(DPI_4))
 c ----------------------------------------------------------------------
 c  Angular conversion factors.
       double precision DAS2R, DD2R, DR2AS, DR2D
@@ -29,36 +29,36 @@ c  Angular conversion factors.
       parameter (DR2AS =  1D0 / DAS2R)
 
       real AS2R, D2R, R2AS, R2D
-      parameter (D2R   = DD2R)
-      parameter (R2D   = DR2D)
-      parameter (AS2R  = DAS2R)
-      parameter (R2AS  = DR2AS)
+      parameter (D2R   = real(DD2R))
+      parameter (R2D   = real(DR2D))
+      parameter (AS2R  = real(DAS2R))
+      parameter (R2AS  = real(DR2AS))
 c ----------------------------------------------------------------------
 c  Speed of light (meter/second).
       double precision DCMKS
       parameter (DCMKS = 299792458D0)
 
       real CMKS
-      parameter (CMKS = DCMKS)
+      parameter (CMKS = real(DCMKS))
 c ----------------------------------------------------------------------
 c  Boltzmann constant (Joule/Kelvin).
       double precision DKMKS
       parameter (DKMKS = 1.380658D-23)
 
       real KMKS
-      parameter (KMKS = DKMKS)
+      parameter (KMKS = real(DKMKS))
 c ----------------------------------------------------------------------
 c  Planck constant (Joule-second).
       double precision DHMKS
       parameter (DHMKS = 6.6260755D-34)
 
       real HMKS
-      parameter (HMKS = DHMKS)
+      parameter (HMKS = real(DHMKS))
 c ----------------------------------------------------------------------
 c  Planck constant divided by Boltzmann constant (Kelvin/GHz).
       double precision DHOVERK
       parameter (DHOVERK = 1D9 * DHMKS / DKMKS)
 
       real HOVERK
-      parameter (HOVERK = DHOVERK)
+      parameter (HOVERK = real(DHOVERK))
 c=======================================================================
