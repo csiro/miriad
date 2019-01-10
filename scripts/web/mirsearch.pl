c476bd35930f592d1e694e8ce3dcc06b4503dad9
Vince McIntyre <Vincent.McIntyre@csiro.au>
2019-01-10 01:51:42 +0000

    search miriad manual pages on the web. As found, 2006-02-20

---
+++ scripts/web/mirsearch.pl
@@ -0,0 +1,193 @@
+#!/usr/local/bin/perl -- -*- perl -*-
+
+# miriadsearch.pl    David Mar  13-Mar-1996
+# modified from: sitesearch.pl    David Mar  29-Sep-1995
+# modified for new server         Henrietta May 12 Mar 1998
+# Processes information from form
+# and produces a list of matching documents.
+
+$TOP='/export/www/vhosts/atnf/htdocs';
+push(@INC, "$TOP/cgi-bin");
+require "www-lib.pl";
+$miriad_dir = "/computing/software/miriad";
+
+sub ReadParse
+{
+  if (@_) {
+    local (*in) = @_;
+  }
+
+  local ($i, $loc, $key, $val);
+
+  # Read in text
+  if ($ENV{'REQUEST_METHOD'} eq "GET") {
+    $in = $ENV{'QUERY_STRING'};
+  } elsif ($ENV{'REQUEST_METHOD'} eq "POST") {
+    for ($i = 0; $i < $ENV{'CONTENT_LENGTH'}; $i++) {
+      $in .= getc;
+    }
+  } 
+
+  @in = split(/&/,$in);
+
+  foreach $i (0 .. $#in) {
+    # Convert plus's to spaces
+    $in[$i] =~ s/\+/ /g;
+
+    # Convert %XX from hex numbers to alphanumeric
+    $in[$i] =~ s/%(..)/pack("c",hex($1))/ge;
+
+    # Split into key and value.
+    $loc = index($in[$i],"=");
+    $key = substr($in[$i],0,$loc);
+    $val = substr($in[$i],$loc+1);
+    $in{$key} .= '\0' if (defined($in{$key})); # \0 is the multiple separator
+    $in{$key} .= $val;
+  }
+
+  return 1; # just for fun
+}
+
+sub searchfile
+{
+	local($dir, $file) = @_;
+        local ($filespec);
+
+#	print "$dir/$file\n";          # DEBUG
+
+	return if ($file !~ /html/);
+	$TitleGo = 0;
+	open(FILE, $file) || die;
+	while (<FILE>)
+	{
+		$Title = $Title.$_ if ($TitleGo);
+		if ($_ =~ /<TITLE/i)
+		{
+			$Title = $_;
+			$TitleGo = 1;
+		}
+		$TitleGo = 0 if ($_ =~ /\/TITLE/i);
+		last if ($_ =~ />Home</);         # Don't search footer links!
+		next if ($_ !~ /$Search/io);
+		$Title =~ s#</*TITLE>|</*title>##g;
+                $filespec = "$miriad_dir/$dir/$file";
+                $filespec = "$miriad_dir/$file" if ($dir = '.');
+		print "<LI><A HREF=\"$filespec\">$Title</A>\n";
+#		print "<LI><A HREF=\"$miriad_dir/$dir/$file\">$Title</A>\n";
+		$Matches++;
+		last;
+	}
+	close(FILE);
+}
+
+sub searchdir
+{
+	local($dir, $nlink) = @_;
+	local($dev, $ino, $mode, $subcount);
+	($dev, $ino, $mode, $nlink) = stat('.') unless $nlink;
+
+	# Get files in current directory.
+	opendir(DIR, '.') || die;
+	local(@filenames) = readdir(DIR);
+	closedir(DIR);
+
+	if ($nlink == 2)            # This dir has no subdirectories.
+	{
+		foreach (@filenames)
+		{
+			next if ($_ eq '.');
+			next if ($_ eq '..');
+			$name = "$dir/$_";
+			&searchfile($dir, $_) if (substr($_, -4) eq 'html');
+		}
+	}
+	else                        # This dir has subdirectories.
+	{
+		$subcount = $nlink - 2;
+		foreach (@filenames)
+        {
+            next if ($_ eq '.');
+            next if ($_ eq '..');
+			$name = "$dir/$_";
+			&searchfile($dir, $_) if (substr($_, -4) eq 'html');
+			next if ($subcount == 0);
+			($dev, $ino, $mode, $nlink) = lstat($_);
+			# Skip unless file is a directory and starts with capital letter.
+			next unless ((-d _) && (ord($_) > 63) && (ord($_) < 95));
+			# Do next directory recursively.
+			chdir($_) || die;
+			&searchdir($name, $nlink);
+			chdir('..');
+			--$subcount;
+		}
+	}
+}
+
+&ReadParse;			# get input;
+
+print << "EOF";
+Content-type: text/html
+
+<HTML><HEAD>
+<TITLE>Miriad Documentation Search Results</TITLE>
+</HEAD>\n<BODY BACKGROUND="/computing/software/miriad/gmiriad.gif">
+<TABLE CELLSPACING="0" CELLPADDING="1"><TR VALIGN=TOP>
+
+<TD WIDTH=160 HEIGHT=800 NOWRAP>
+<BR><BR><BR><BR><BR><BR><BR><BR><BR>
+<BR><BR>
+
+<!-- General Search -->
+
+<FORM ACTION="/cgi-bin/miriad/mirsearch.pl" METHOD=POST>
+<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" NOWRAP>
+<TR><TH ALIGN=LEFT COLSPAN=2><FONT COLOR="#FFFFFF"><B>Search</B></FONT></TH></TR>
+<TR><TD><INPUT TYPE="Text" NAME="searchstring" SIZE="10" MAXLENGTH="50"></TD>
+    <TD><INPUT TYPE="Image" NAME="Go" SRC="/computing/software/miriad/gblue.gif" BORDER="0"></TD></TR>
+</TABLE>
+</FORM>
+<BR>
+
+<!-- Task Lookup -->
+
+<FORM ACTION="/cgi-bin/miriad/mirman.pl" METHOD=GET>
+<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" NOWRAP>
+<TR><TH ALIGN=LEFT COLSPAN=2><FONT COLOR="#FFFFFF"><B>Topic Lookup</B></FONT></TH></TR>
+<TR><TD><INPUT TYPE="Text" NAME="topic" SIZE="10" MAXLENGTH="50"></TD>
+    <TD><INPUT TYPE="Image" NAME="Go" SRC="/computing/software/miriad/gblue.gif" BORDER="0"></TD></TR>
+</TABLE>
+</FORM>
+
+
+</TD><TD>
+<BR>
+<CENTER><H1><I>Miriad</I> Documentation Search Results</H1></CENTER>
+<HR>
+EOF
+
+$Matches = 0;
+
+
+$Search = $in{'searchstring'};
+print "Your search string: $Search<P>\n";
+if ($Search =~ /^\s*$/)         # Search string is white space or blank.
+{
+	print "<STRONG>Please enter a search string.</STRONG>\n";
+}
+else                            # Search string contains something.
+{
+	print "<UL>\n";
+	chdir("$TOP/$miriad_dir");
+	&searchdir('.');
+	print "</UL>\n";
+
+	if ($Matches == 0)
+	{
+		print "<STRONG>Sorry. No matches.</STRONG><BR>\n";
+		print "You may wish to go back and try again with a slightly ";
+		print "different search string.\n";
+	}
+}
+
+print "<HR><EM>miriad\@atnf.csiro.au</EM></TD></TR></TABLE></BODY></HTML>\n";
+# end
